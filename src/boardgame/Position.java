9fb26c11b28cfc2ad5edcaed4f5880439242086c
MatheusMOAndrade <andradematheusmo@gmail.com>
2024-08-14 17:23:03 -0300

    add: attributes and methods - Position, Board, Piece class

--- src/boardgame/Position.java
+++ src/boardgame/Position.java
@@ -5,8 +5,31 @@ public class Position {
     private int row;
     private int column;
 
-    public void setValues(int row, int column) {
+    /* Contructors */
+    public Position(int row, int column) {
+        this.row = row;
+        this.column = column;
+    }
 
+    /* Getters and Setters */
+    public int getRow() {
+        return row;
+    }
+
+    public void setRow(int row) {
+        this.row = row;
+    }
+
+    public int getColumn() {
+        return column;
+    }
+
+    public void setColumn(int column) {
+        this.column = column;
     }
     
+    @Override
+    public String toString() {
+        return row + ", " + column;
+    }
 }
