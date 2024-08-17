f601e20a2c5f611d0ea3ccab51ace58ed84b52d8
MatheusMOAndrade <andradematheusmo@gmail.com>
2024-08-16 22:20:31 -0300

    add: check logic

--- src/application/App.java
+++ src/application/App.java
@@ -2,6 +2,9 @@ package application;
 
 import java.util.Scanner;
 import java.util.InputMismatchException;
+import java.util.List;
+import java.util.ArrayList;
+
 import chess.ChessException;
 import chess.ChessMatch;
 import chess.ChessPiece;
@@ -12,13 +15,14 @@ public class App {
 
         Scanner sc = new Scanner(System.in);
         ChessMatch chessMatch = new ChessMatch();
+        List<ChessPiece> captured = new ArrayList<>();
 
         //move pieces logic
         while (true) {
             try {
                 UI.clearScreen();
 
-                UI.printBoard(chessMatch.getPieces());
+                UI.printMatch(chessMatch, captured);
                 System.out.println();
                 System.out.print("Source: ");
                 ChessPosition source = UI.readChessPosition(sc);
@@ -32,6 +36,10 @@ public class App {
                 ChessPosition target = UI.readChessPosition(sc);
 
                 ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+
+                if(capturedPiece != null) {
+                    captured.add(capturedPiece);
+                }
             }
             catch (ChessException e) {
                 System.out.println(e.getMessage());
